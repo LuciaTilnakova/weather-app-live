d17c75013fb03b7c12bee58ad61914522d95ad8e
Lucia Tilnakova <lucia.tilki@gmail.com>
2021-05-28 13:58:47 +0100

    Update time

--- src/script.js
+++ src/script.js
@@ -1,8 +1,30 @@
 let apiKey = "90d97642321171723050e9abe037b905";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
@@ -18,6 +40,9 @@ function showTemperature(response) {
   let windElement = document.querySelector("#wind");
   let windSpeed = Math.round(response.data.wind.speed);
   windElement.innerHTML = `Wind: ${windSpeed} km/h`;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 axios.get(apiUrl).then(showTemperature);
